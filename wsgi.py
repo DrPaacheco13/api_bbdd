8dac7e5a803e34816cd4b2934a78f636fb470e7a
DrPaacheco13 <dretamal@dmtech.cl>
2024-02-22 10:29:56 -0300

    avances

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
 from main import app
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    httpd = make_server('localhost', 5000, app)
+    print("Servidor HTTP en ejecución en http://localhost:5000...")
+    httpd.serve_forever()
+
